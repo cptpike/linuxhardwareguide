0cf61bfed797506f7ac56218b9ed249da11373e7
cptpike <linux.hardware.guide.en@gmail.com>
2016-01-30 17:36:43 +0100

    runs sanity checks of price database and performs corrections if necessary

---
+++ wp-content/plugins/lhg-pricedb/includes/lhg_sanity_checks.php
@@ -0,0 +1,102 @@
+<?php
+
+function lhg_sanity_checks ( ) {
+        echo "Sanity Checks";
+
+        echo "<h2>Test 1: Redcoon Products</h2>";
+        echo "Sanity Check:<br>Look for Products at Redcoon.de, copy to .it & .nl<br>";
+        lhg_sanity_redcoon();
+}
+
+function lhg_sanity_redcoon ( ) {
+        #echo "Searching for SID 1";
+
+
+        $sid = 1;
+    	global $lhg_price_db;
+    	$sql = "SELECT lhg_article_id FROM `lhgprices` WHERE `shop_id` = 1 AND `shop_article_id` != 'NOT_AVAILABLE'";
+    	$results = $lhg_price_db->get_results($sql);
+
+        #print "<br>Results: ";
+        #var_dump($results);
+        #return $result;
+
+        foreach ($results as $result) {
+                $print_me = 0;
+
+	    	$sql = "SELECT shop_article_id FROM `lhgprices` WHERE `shop_id` = 1 AND `lhg_article_id` = ".$result->lhg_article_id;
+    		$shop_article_id = $lhg_price_db->get_var($sql);
+
+	    	$sql = "SELECT permalink_de FROM `lhgtransverse_posts` WHERE `postid_de` = ".$result->lhg_article_id;
+    		$shop_article_url_de = $lhg_price_db->get_var($sql);
+
+	    	$sql = "SELECT permalink_com FROM `lhgtransverse_posts` WHERE `postid_com` = ".$result->lhg_article_id;
+    		$shop_article_url_com = $lhg_price_db->get_var($sql);
+
+                #print "<br>Res: "; var_dump($result);
+                if ($shop_article_url_de != "") $output = '<br>ID_de: <a href="'.$shop_article_url_de.'">'. $result->lhg_article_id ."</a>";
+                if ($shop_article_url_de == "") $output = '<br>ID_de: '. $result->lhg_article_id;
+
+	    	$sql = "SELECT postid_com FROM `lhgtransverse_posts` WHERE `postid_de` = ".$result->lhg_article_id;
+    		$pid_com = $lhg_price_db->get_var($sql);
+                if ($shop_article_url_com != "") $output .= ' -> ID_com: <a href="'.$shop_article_url_com.'">'.$pid_com."</a>";
+                if ($shop_article_url_com == "") $output .= ' -> ID_com: '.$pid_com;
+
+                if ($pid_com != 0) {
+                  #check if available
+                  $output .= "-> check if already available (it:";
+
+		  $sql = "SELECT id FROM `lhgprices` WHERE `lhg_article_id` = ".$pid_com." AND shop_id = 5";
+    		  $price_id_it = $lhg_price_db->get_var($sql);
+
+                  if ($price_id_it != "") $output .= " $price_id_it) ";
+                  if ($price_id_it == "") $output .= " none) ";
+                  if ($price_id_it == "") $print_me = 1;
+
+                  $output .= " (nl:";
+		  $sql = "SELECT id FROM `lhgprices` WHERE `lhg_article_id` = ".$pid_com." AND shop_id = 15";
+    		  $price_id_nl = $lhg_price_db->get_var($sql);
+
+                  if ($price_id_nl != "") $output .= " $price_id_nl) ";
+                  if ($price_id_nl == "") $output .= " none) ";
+                  if ($price_id_nl == "") $print_me = 1;
+
+
+                  #create entry
+                  if ($price_id_it == "") {
+			$output .= "<br>&nbsp;&nbsp;-> create entry IT ";
+
+                        $shop_id = 5;
+                        $post_id = $pid_com;
+			$result = lhg_db_create_entry( $post_id, $shop_id, $shop_article_id );
+			$output .= ".. done";
+			//get newly created lhg_db_id
+			$lhg_db_id = lhg_db_get_id ( $post_id, $shop_id);
+		        lhg_refresh_database_entry($lhg_db_id,$shop_id);
+                        $print_me = 1;
+	          }
+                  if ($price_id_nl == "") {
+			$output .= "<br>&nbsp;&nbsp;-> create entry NL ";
+
+                        $shop_id = 15;
+                        $post_id = $pid_com;
+			$result = lhg_db_create_entry( $post_id, $shop_id, $shop_article_id );
+			$output .= ".. done";
+			//get newly created lhg_db_id
+			$lhg_db_id = lhg_db_get_id ( $post_id, $shop_id);
+		        lhg_refresh_database_entry($lhg_db_id,$shop_id);
+                        $print_me = 1;
+	          }
+
+
+
+		}else{
+                  $output .= "-> Product not found in COM DB";
+                  $print_me = 1;
+
+                }
+
+        if ($print_me == 1) print $output;
+	}
+}
+
