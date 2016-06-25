06f9ced3b3649dee379ab64b8935c0e9db331de6
cptpike <linux.hardware.guide.en@gmail.com>
2016-06-25 21:08:50 +0200

    Introduction of progress par to indicate server side processing of data

--- wp-content/plugins/lhg-hardware-profile-manager/templates/scan.php
+++ wp-content/plugins/lhg-hardware-profile-manager/templates/scan.php
@@ -118,6 +118,10 @@ if ($show_public_profile) {
 }
 
 
+# check if this is the first visit or upload was performed recently
+# In this case the DB server is still processing and we need to wait a little
+lhg_check_if_recent_upload( $sid );
+
 # store login data
 # we need this info to link scan results with users
 # and ratings with kernel versions
@@ -2588,6 +2592,206 @@ function lhg_get_mainboard_name_from_DMI ( $dmesg ) {
         #print "<br>";
 
         return $mbstring;
+}
+
+function lhg_check_if_recent_upload ( $sid ) {
+
+	global $lhg_price_db;
+
+        # look when scan was uploaded
+        $myquery = $lhg_price_db->prepare("SELECT scandate FROM `lhgscansessions` WHERE sid = %s", $sid);
+        $scandate = $lhg_price_db->get_var($myquery);
+
+        # look if already someone logged in
+        #$myquery = $lhg_price_db->prepare("SELECT date FROM `lhgscan_login` WHERE sid = %s", $sid);
+        #$logindate = $lhg_price_db->get_var($myquery);
+
+        $diff = time() - $scandate;
+
+        if ($diff < 15) {
+
+        # The data was uploaded less than 15 seconds ago
+        # in order to keep the visitor informed that something is ongoing on server side
+        # we show some progress bar and forward to the real page after 15 seconds
+
+print '
+<html>
+
+<head>
+	<meta charset=\'UTF-8\'>
+        <meta http-equiv="refresh" content="15">
+	
+	<title>Progressing Data - Please wait</title>
+	
+	<link rel=\'stylesheet\' href=\'css/style.css\'>
+	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+	<script>
+		$(function() {
+			$(".meter > span").each(function() {
+				$(this)
+					.data("origWidth", $(this).width())
+					.width(0)
+					.animate({
+						width: $(this).data("origWidth")
+					}, 15000);
+			});
+		});
+	</script>
+	
+	<style>
+		.meter { 
+			height: 20px;  /* Can be anything */
+			position: relative;
+			margin: 5px 0 20px 0; /* Just for demo spacing */
+			background: #555;
+			-moz-border-radius: 25px;
+			-webkit-border-radius: 25px;
+			border-radius: 25px;
+			padding: 10px;
+			-webkit-box-shadow: inset 0 -1px 1px rgba(255,255,255,0.3);
+			-moz-box-shadow   : inset 0 -1px 1px rgba(255,255,255,0.3);
+			box-shadow        : inset 0 -1px 1px rgba(255,255,255,0.3);
+                        width: 50%;
+                        margin-left: auto;
+                        margin-right: auto;
+		}
+		.meter > span {
+			display: block;
+			height: 100%;
+			   -webkit-border-top-right-radius: 8px;
+			-webkit-border-bottom-right-radius: 8px;
+			       -moz-border-radius-topright: 8px;
+			    -moz-border-radius-bottomright: 8px;
+			           border-top-right-radius: 8px;
+			        border-bottom-right-radius: 8px;
+			    -webkit-border-top-left-radius: 20px;
+			 -webkit-border-bottom-left-radius: 20px;
+			        -moz-border-radius-topleft: 20px;
+			     -moz-border-radius-bottomleft: 20px;
+			            border-top-left-radius: 20px;
+			         border-bottom-left-radius: 20px;
+			background-color: rgb(43,194,83);
+			background-image: -webkit-gradient(
+			  linear,
+			  left bottom,
+			  left top,
+			  color-stop(0, rgb(43,194,83)),
+			  color-stop(1, rgb(84,240,84))
+			 );
+			background-image: -moz-linear-gradient(
+			  center bottom,
+			  rgb(43,194,83) 37%,
+			  rgb(84,240,84) 69%
+			 );
+			-webkit-box-shadow: 
+			  inset 0 2px 9px  rgba(255,255,255,0.3),
+			  inset 0 -2px 6px rgba(0,0,0,0.4);
+			-moz-box-shadow: 
+			  inset 0 2px 9px  rgba(255,255,255,0.3),
+			  inset 0 -2px 6px rgba(0,0,0,0.4);
+			box-shadow: 
+			  inset 0 2px 9px  rgba(255,255,255,0.3),
+			  inset 0 -2px 6px rgba(0,0,0,0.4);
+			position: relative;
+			overflow: hidden;
+		}
+		.meter > span:after, .animate > span > span {
+			content: "";
+			position: absolute;
+			top: 0; left: 0; bottom: 0; right: 0;
+			background-image: 
+			   -webkit-gradient(linear, 0 0, 100% 100%, 
+			      color-stop(.25, rgba(255, 255, 255, .2)), 
+			      color-stop(.25, transparent), color-stop(.5, transparent), 
+			      color-stop(.5, rgba(255, 255, 255, .2)), 
+			      color-stop(.75, rgba(255, 255, 255, .2)), 
+			      color-stop(.75, transparent), to(transparent)
+			   );
+			background-image: 
+				-moz-linear-gradient(
+				  -45deg, 
+			      rgba(255, 255, 255, .2) 25%, 
+			      transparent 25%, 
+			      transparent 50%, 
+			      rgba(255, 255, 255, .2) 50%, 
+			      rgba(255, 255, 255, .2) 75%, 
+			      transparent 75%, 
+			      transparent
+			   );
+			z-index: 1;
+			-webkit-background-size: 50px 50px;
+			-moz-background-size: 50px 50px;
+			-webkit-animation: move 2s linear infinite;
+			   -webkit-border-top-right-radius: 8px;
+			-webkit-border-bottom-right-radius: 8px;
+			       -moz-border-radius-topright: 8px;
+			    -moz-border-radius-bottomright: 8px;
+			           border-top-right-radius: 8px;
+			        border-bottom-right-radius: 8px;
+			    -webkit-border-top-left-radius: 20px;
+			 -webkit-border-bottom-left-radius: 20px;
+			        -moz-border-radius-topleft: 20px;
+			     -moz-border-radius-bottomleft: 20px;
+			            border-top-left-radius: 20px;
+			         border-bottom-left-radius: 20px;
+			overflow: hidden;
+		}
+		
+		.animate > span:after {
+			display: none;
+		}
+		
+		@-webkit-keyframes move {
+		    0% {
+		       background-position: 0 0;
+		    }
+		    100% {
+		       background-position: 50px 50px;
+		    }
+		}
+		
+		.orange > span {
+			background-color: #f1a165;
+			background-image: -moz-linear-gradient(top, #f1a165, #f36d0a);
+			background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #f1a165),color-stop(1, #f36d0a));
+			background-image: -webkit-linear-gradient(#f1a165, #f36d0a); 
+		}
+		
+		.red > span {
+			background-color: #f0a3a3;
+			background-image: -moz-linear-gradient(top, #f0a3a3, #f42323);
+			background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #f0a3a3),color-stop(1, #f42323));
+			background-image: -webkit-linear-gradient(#f0a3a3, #f42323);
+		}
+		
+		.nostripes > span > span, .nostripes > span:after {
+			-webkit-animation: none;
+			background-image: none;
+		}
+                .text {
+                        margin-top: 20px;
+                        text-align: center;
+                }
+
+	</style>
+</head>
+
+<body>
+
+<div class="text">Please wait while the server is processing your data</div>
+
+<div class="meter animate">
+	<span style="width: 100%"><span></span></span>
+</div>
+</body>
+</html>
+';
+                exit;
+	}
+
+        # no need to let the visitor wait
+        return;
 }
 
 ?>
